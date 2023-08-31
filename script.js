032921d6b0147b616697adda7528ab0bc63272a5
Michal Kurek (Mike) <michal@kurek.pl>
2023-08-31 16:12:07 +0200

    added searchTask function - yet to be fixed

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const addFormInput = document.querySelector(".add-form input");
+const searchForm = document.querySelector(".search-form input");
 const submitBtn = document.querySelector(".submitBtn");
 const ul = document.querySelector("ul");
 const taskCount = document.querySelector("h2 span");
 const liItems = document.getElementsByClassName("task");
+
+const searchTask = (e) => {
+  e.preventDefault();
+  const inputText = e.target.value.toLowerCase();
+  let tasks = [...liItems];
+  tasks = tasks.filter((index) => index.textContent.toLowerCase().includes(inputText));
+  ul.textContent = "";
+  // console.log(tasks);
+  tasks.forEach((index) => ul.appendChild(index));
+  taskCount.textContent = liItems.length;
+};
 
 const removeTask = (e) => {
   e.target.parentNode.remove();
@@ -16,10 +28,12 @@ const addTask = (e) => {
   const li = document.createElement("li");
   li.innerHTML = newTask + "<button>delete</button>";
   li.className = "task";
+  // tasks.push(li);
   ul.appendChild(li);
   addFormInput.value = "";
-  taskCount.textContent = liItems.length;
+  taskCount.textContent = document.querySelectorAll("li.task").length;
   li.querySelector("button").addEventListener("click", removeTask);
 };
 
 submitBtn.addEventListener("click", addTask);
+searchForm.addEventListener("input", searchTask);
