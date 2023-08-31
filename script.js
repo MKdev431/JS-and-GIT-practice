2f62bce569a9595e3b2a293556cbf399ad6003ca
Michal Kurek (Mike) <michal@kurek.pl>
2023-08-31 13:53:49 +0200

    added removeTask function

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const addFormInput = document.querySelector(".add-form input");
 const submitBtn = document.querySelector(".submitBtn");
 const ul = document.querySelector("ul");
 const taskCount = document.querySelector("h2 span");
-const liItems = document.querySelectorAll("li");
+const liItems = document.getElementsByClassName("task");
+
+const removeTask = (e) => {
+  e.target.parentNode.remove();
+  taskCount.textContent = liItems.length;
+};
 
 const addTask = (e) => {
   e.preventDefault();
   const newTask = addFormInput.value;
+  if (newTask === "") return;
   const li = document.createElement("li");
   li.innerHTML = newTask + "<button>delete</button>";
+  li.className = "task";
   ul.appendChild(li);
+  addFormInput.value = "";
   taskCount.textContent = liItems.length;
+  li.querySelector("button").addEventListener("click", removeTask);
 };
 
 submitBtn.addEventListener("click", addTask);
